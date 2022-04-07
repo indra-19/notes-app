e81cd71e636dbb85cd1954f236e3c3a509b6048b
indra-11 <galunggungone@gmail.com>
2022-04-08 05:20:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
 
 function App() {
+  const [cookies, setCookie] = useCookies(['acts'])
+  const [names, setNames] = useState([])
+  const activity = useRef()
+
+  useEffect(() => {
+    setNames(cookies.acts)
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    setCookie('acts', JSON.stringify(names), { path: '/', expires: new Date(Date.now()+2592000) });
+  }, [names]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const add = () => {
+    if (activity.current.value && !names.includes(activity.current.value)) {
+      setNames([...names, activity.current.value]);
+      activity.current.value = '';
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="note">
+      <div className="topbar">
+        <input className="add" type="text" placeholder="Note here" ref={activity} autoComplete="off" />
+        <button className="btn" onClick={() => add()}>
+          <span className="material-icons-outlined">add</span>
+        </button>
+      </div>
+      {names.length === 0 ? <p className="empty">Your note is empty</p> :
+      names.map(name => 
+        <div className="box">
+            <p className="text">{name}</p>
+            <button className="btn" onClick={() => setNames(names.filter(name1 => name1 !== name))}>
+              <span className="material-icons-outlined">delete</span>
+            </button>
+        </div>
+      )}
     </div>
   );
 }
